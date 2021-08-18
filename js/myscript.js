5be9a52bf0cc8a44b2d8293efdb7f97143d2f95a
marco2541 <marcocapa2541@gmail.com>
2021-08-18 12:47:10 +0200

    bien

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,70 @@
 var time = new Date();
 console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
 
-document.addEventListener("click", function(event){
-    let boton= event.target.dataset.boton;
-    let sonido
+// document.addEventListener("click", function(event){
+//     let boton = event.target.dataset.boton;
+//     let sonido = do
+
+// }
+
+function playredoble(){
+       
+       let audio= document.getElementById("redoble");
+       audio.curretTime=0;
+        audio.play();
 
 }
+
+    function playredoble(){
+        
+        let audio= document.getElementById("redoble");
+        audio.curretTime=0;
+        audio.play();
+
+    }
+
+        function playredoble(){
+        
+            let audio= document.getElementById("redoble");
+            audio.curretTime=0;
+            audio.play();
+    
+    }
+            function playredoble(){
+            
+                let audio= document.getElementById("redoble");
+                audio.curretTime=0;
+                audio.play();
+        
+        }
+                function playredoble(){
+            
+                    let audio= document.getElementById("redoble");
+                    audio.curretTime=0;
+                    audio.play();
+            
+            }
+                    function playredoble(){
+            
+                        let audio= document.getElementById("redoble");
+                        audio.curretTime=0;
+                        audio.play();
+                
+                }
+                        function playredoble(){
+                    
+                            let audio= document.getElementById("redoble");
+                            audio.curretTime=0;
+                            audio.play();
+                    
+                    }
+                            function playredoble(){
+                            
+                                let audio= document.getElementById("redoble");
+                                audio.curretTime=0;
+                                audio.play();
+                        
+                        }
+
+
+
